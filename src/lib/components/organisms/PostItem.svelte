d65024edd272bec2e8743942030c60de50654495
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-10-17 19:59:37 +0900

    記事更新フォームとリンクを実装 (#11)

--- src/lib/components/organisms/PostItem.svelte
+++ src/lib/components/organisms/PostItem.svelte
@@ -6,7 +6,8 @@
 
 <article class="border p-2">
   <a href={`${post.user.name}/posts/${post.id}`}><h1 class="font-bold">{post.title}</h1></a>
-  <nav>
-    <p class="flex justify-between">by {post.user.name}</p>
+  <nav class="flex justify-between">
+    by {post.user.name}
+    <a href={`${post.user.name}/posts/${post.id}/edit`} class="text-green-500">更新</a>
   </nav>
 </article>
---
+++ src/routes/[name]/posts/[id]/edit/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+
+</script>
+
+<svelte:head>
+  <title>記事編集フォーム - SvelteLaravelBlogApp</title>
+</svelte:head>
+
+<h1 class="font-bold">記事更新フォーム</h1>
