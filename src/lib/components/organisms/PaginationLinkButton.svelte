13992bdaf5771f43f6d006d3f673895d1dd32362
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-10-23 15:39:55 +0900

    ページネーション機能の実装 (#15)

---
+++ src/lib/components/organisms/PaginationLinkButton.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let posts: any;
+</script>
+
+{#each posts.links as link}
+  <a 
+    href={Number(link.label) ? `/?page=${link.label}`  : link.label.includes("前") ? `/?page=${posts.current_page - 1}`  : `/?page=${posts.current_page + 1}`} 
+    class={`${link.active && "bg-blue-500 pointer-events-none"} ${(!posts.prev_page_url && link.label.includes("前")) && "hidden"} ${(!posts.next_page_url && link.label.includes("次")) && "hidden"} font-bold border p-4`}>
+      {@html link.label}
+  </a>
+  {/each}
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,9 @@
 import type { PageServerLoad } from "./$types"
 import { axios } from "$lib/axios";
 
-export const load: PageServerLoad = async ({ parent }) => {
+export const load: PageServerLoad = async ({ parent, url }) => {
   const { user } = await parent();
-  const res = await axios.get(`${import.meta.env.VITE_API_URL}/api/posts`);
+  const res = await axios.get(`${import.meta.env.VITE_API_URL}/api/posts/${url.search}`);
   const posts = await res.data;
 
   return {
