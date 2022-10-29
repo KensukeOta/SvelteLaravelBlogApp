0fab4fb2d7dfb3cb3176342a468dc7b98821f5c6
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-10-29 20:20:17 +0900

    SvelteKitを1.0.0-next.527にダウングレード

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,7 +3,7 @@ import { axios } from "$lib/axios";
 
 export const load: PageServerLoad = async ({ parent, url }) => {
   const { user } = await parent();
-  const res = await axios.get(`${import.meta.env.VITE_API_URL}/api/posts/${url.search}`);
+  const res = await axios.get(`${import.meta.env.VITE_API_URL}/api/posts${url.search}`);
   const posts = await res.data;
 
   return {
