fdc9ef80687cdfcc46b1fceabfea3ad5a38dc426
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-11-01 20:34:35 +0900

    記事検索の入力フォームを作成し、ヘッダーに設置 (#17)

---
+++ src/lib/components/atoms/SearchInput.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import { searchKeyword } from "$lib/stores/searchKeyword"
+</script>
+
+<input type="text" name="search" id="search" bind:value={$searchKeyword} placeholder="記事を検索" class="border" />
